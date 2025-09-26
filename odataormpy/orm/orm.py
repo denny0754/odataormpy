138aaaf55a08c09ba37801dd8f6cf7c61b2eb11c
Diego Vaccher <dvaccher99@gmail.com>
2025-09-26 16:30:37 +0200

    upd: improves Metadata parsing and storage

--- odataormpy/orm/orm.py
+++ odataormpy/orm/orm.py
@@ -13,6 +13,7 @@ from functools import lru_cache
 import xml.etree.ElementTree as ET
 
 from odataormpy.session import ORMSession
+from .orm_metadata import ORMMetadata
 
 class ORM:
     """Defines the actual ORM object used to interact with OData services.
@@ -23,86 +24,8 @@ class ORM:
         :param session: ORM Session used to interact with OData services/source system.
         """
         self.orm_session = session
-        '''
-            Example __service structure:
-            {
-                "CorporateAccount": {
-                    "attributes": {
-                        "endpoint": "/sap/odata/v2",
-                        "updatable": True,
-                        "creatable": True,
-                        "deletable": False
-                    },
-                    "properties": {
-                        "ObjectID": {
-                            "key": True,
-                            "max_length": 70,
-                            "data_type": "Edm.String",
-                            "nullable": False
-                        }
-                    }
-                }
-            }
-        '''
-        self.__service : dict = { }
 
-    def __parse_service_metadata(self,
-                                 raw_metadata : str,
-                                 service_name : str,
-                                 lazy_load : bool) -> None:
-        """Parses the metadata response and sets up the service structure for
-        easy access.
-
-        :param raw_metadata: Raw metadata response from OData service.
-        :param service_name: Name of the service requested.
-        :return:
-        """
-        #TODO: Update the structure of the service metadata
-        #TODO: Use `lazy_load` parameter to disable loading of metadata and store them
-        #      as lzma payloads to save space.
-
-        xml_ns = {
-            "edmx": "http://schemas.microsoft.com/ado/2007/06/edmx",
-            "m": "http://schemas.microsoft.com/ado/2007/08/dataservices/metadata",
-            "edm": "http://schemas.microsoft.com/ado/2008/09/edm",
-            "sap": "http://www.sap.com/Protocols/SAPData"
-        }
-
-        self.__service[service_name] = {
-            "entity_props": { },
-            "entity_attrs": { }
-        }
-
-        xml_root = ET.fromstring(raw_metadata)
-
-        # Getting all entities and their attributes
-        entity_container = xml_root.find(".//edm:EntityContainer", xml_ns)
-        for entity in entity_container.findall("edm:EntitySet", xml_ns):
-            e_name = entity.get("Name")
-            e_type = entity.get("EntityType")
-            #!TODO: Using the dynamic way, we would get "sap:creatable", "sap:updatable"
-            #       and "sap:deletable".
-            #       I should map these in a way I'm in charge of the key on my dictionary.
-            #       Non priority. This is ok at the moment.
-
-            self.__service[service_name]["attributes"][e_name] = {
-                "type": e_type,
-                "updatable": entity.get('{http://www.sap.com/Protocols/SAPData}updatable', False),
-                "creatable": entity.get('{http://www.sap.com/Protocols/SAPData}updatable', False),
-                "deletable": entity.get('{http://www.sap.com/Protocols/SAPData}updatable', False)
-            }
-
-        # Getting all entities and their properties
-        for entity in xml_root.findall(".//edm:EntityType", xml_ns):
-            e_name = entity.attrib.get("Name")
-
-            for prop in entity.findall("edm:Property", xml_ns):
-                ep_name = prop.attrib.get("Name")
-                self.__service[service_name]["properties"][e_name][ep_name] = {
-                    "data_type": prop.attrib.get("Type"),
-                    "nullable": prop.attrib.get("Nullable"),
-                    "max_length": prop.attrib.get("MaxLength")
-                }
+        self.__services : dict = { }
 
     def register_service(self,
                          service_name : str,
@@ -119,35 +42,7 @@ class ORM:
         :param lazy_load: Lazy-loading of the metadata structure.
         :return: None
         """
-        #!TODO: lazy_load should be passed to the parser and used to compress
-        #       the metadata for the objects. This will save a little bit of
-        #       memory space in case the metadata file is huge.
         if self.orm_session:
             metadata_response = self.orm_session.get(f'{service_endpoint}/$metadata', None)
             if metadata_response.ok:
-                self.__parse_service_metadata(
-                    raw_metadata=metadata_response.content.decode('utf-8'),
-                    service_name=service_name,
-                    lazy_load=lazy_load
-                )
-                self.__service[service_name]["endpoint"] = service_endpoint
-
-    @lru_cache(maxsize=256)
-    def get_entity_metadata(self, entity : str) -> dict:
-        """Returns the metadata for a given entity.
-
-        :param entity: Entity to get metadata for.
-        :return: Entity metadata.
-        """
-        return self.__service.get(entity, { })
-
-    @lru_cache(maxsize=256)
-    def list_entities(self, service_name : str) -> list[str]:
-        """Returns a list of all entities for a given service name.
-
-        :param service_name: Name of the service requested.
-        :return: List of entities.
-        """
-        entities = list(self.__service.get(service_name, {}).get("attributes",{}).keys())
-
-        return entities
+                self.__services[service_name] = ORMMetadata(metadata_response, lazy_load)
---
+++ odataormpy/orm/orm_metadata.py
@@ -0,0 +1,145 @@
+"""
+Author: Diego Vaccher <dvaccher99@gmail.com>
+Created on: 2025-09-26
+
+Description:
+    Provides a storage class OData service metadata.
+
+Change Log:
+    2025-09-26 - Diego Vaccher - Initial creation
+"""
+
+import xml.etree.ElementTree as ET
+import lzma
+import json
+
+class ORMMetadata:
+    """ORM Metadata storage.
+
+    """
+    def __init__(self, xml, lazy_load : bool = True) -> None:
+        """
+
+        :param xml: Raw XML data to parse
+        :param lazy_load: Lazy loads metadata from XML.
+        """
+        self.__xml_data : str = xml
+
+        self.__xml_namespaces : dict = {
+            "edmx": "http://schemas.microsoft.com/ado/2007/06/edmx",
+            "m": "http://schemas.microsoft.com/ado/2007/08/dataservices/metadata",
+            "edm": "http://schemas.microsoft.com/ado/2008/09/edm",
+            "sap": "http://www.sap.com/Protocols/SAPData"
+        }
+
+        self.__lazy_load : bool = lazy_load
+        self.__entities : dict = { }
+
+        self.__parse_xml()
+
+    def __parse_attribute(self, entity_attribute):
+        """Parses all attributes of the current entity.
+
+        :param entity_attribute: XML attributes to parse.
+        :return:
+        """
+        entity_name = entity_attribute.get("Name", "")
+
+        if entity_name in self.__entities or entity_name == "":
+            return
+
+        self.__entities[entity_name] = {
+            "attributes": {}
+        }
+
+        entity_type = entity_attribute.get("Type", "")
+
+        self.__entities[entity_name]["attributes"] = {
+            "type": entity_type,
+            "updatable": entity_attribute.get(
+                '{http://www.sap.com/Protocols/SAPData}updatable', False),
+            "creatable": entity_attribute.get(
+                '{http://www.sap.com/Protocols/SAPData}creatable', False),
+            "deletable": entity_attribute.get(
+                '{http://www.sap.com/Protocols/SAPData}deletable', False),
+        }
+
+    def __parse_property(self, entity_property):
+        """Parses all properties of the current entity.
+
+        :param entity_property: XML property to parse.
+        :return:
+        """
+        entity_name = entity_property.attrib.get("Name")
+        for prop in entity_property.findall("edm:Property", self.__xml_namespaces):
+            ep_name = prop.attrib.get("Name")
+            self.__entities[entity_name]["properties"][ep_name] = {
+                "data_type": prop.attrib.get("Type"),
+                "nullable": prop.attrib.get("Nullable"),
+                "max_length": prop.attrib.get("MaxLength")
+            }
+
+    def __compress_lazy(self) -> None:
+        """Compresses all entities using LZMA compression.
+
+        :return:
+        """
+        for key in self.__entities.keys():
+            data = json.dumps(self.__entities[key])
+            compress_data = lzma.compress(data.encode('utf-8'))
+            self.__entities[key].pop("attributes")
+            self.__entities[key].pop("properties")
+            self.__entities[key] = {
+                "__lazy_loaded": compress_data
+            }
+
+    def __decompress_lazy(self, entity_name) -> None:
+        """Decompress lazy data for the given entity.
+
+        :param entity_name: Entity name to decompress
+        :return:
+        """
+        if entity_name in self.__entities:
+            data = self.__entities[entity_name]["__lazy_loaded"]
+            decompressed_data = lzma.decompress(data)
+            self.__entities[entity_name].pop("__lazy_loaded")
+            self.__entities[entity_name] = json.loads(decompressed_data)
+
+    def __parse_xml(self):
+        """Parses the XML raw data and saves it on this storage class.
+
+        :return: None
+        """
+        xml_root = ET.fromstring(self.__xml_data)
+
+        entity_container = xml_root.find(".//edm:EntityContainer", self.__xml_namespaces)
+
+        for entity in entity_container.findall("edm:EntitySet", self.__xml_namespaces):
+            self.__parse_attribute(entity)
+
+        for entity in xml_root.findall(".//edm:EntityType", self.__xml_namespaces):
+            self.__parse_property(entity)
+
+        if self.__lazy_load:
+            self.__compress_lazy()
+
+    def __getitem__(self, key: str):
+        """Allow read-only access to entities using ['entity_name'].
+
+        :param key: Entity name
+        """
+        # Check if the key exists in the entities
+        if key in self.__entities:
+            # If lazy-loaded, decompress before returning
+            if "__lazy_loaded" in self.__entities[key]:
+                self.__decompress_lazy(key)
+            return self.__entities[key]
+        else:
+            raise KeyError(f"Entity '{key}' not found.")
+
+    def __repr__(self) -> str:
+        """String representation of an entity
+
+        :return: A string representation of the object.
+        """
+        return json.dumps(self.__entities, indent=4)
